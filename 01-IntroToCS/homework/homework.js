710fbf633d6ca1d625cbe7bc5d677f2409bd8a42
DiegoCaceresC <diego.caceres.canova@gmail.com>
2023-02-28 09:26:33 -0500

    ejercicio 1 terminado

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,15 +1,14 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let numstr=parseInt(num);
+   let dec = parseInt(numstr, 2);
+   return dec;
+}
 
 function DecimalABinario(num) {
-   let number = num;
-   let binary = (number % 2).toString();
-   for (; number > 1; ) {
-       number = parseInt(number / 2);
-       binary =  (Number % 2) + (binary);
-   }
-   return binary;
+   let str = (num).toString(2);
+   return str;
 }
 
 module.exports = {
